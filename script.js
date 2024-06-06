dc478fe670c165be6bd04072a413da50792a83b4
AgustinM12 <agustinmazza12345@gmail.com>
2024-06-06 16:44:23 -0300

    FEAT: se agregaron comentarios explicativos

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// * los canvas son un elemento de html que no poseen contendio propio, por lo que requieren de js para dibujar contenido en el.
 const canvas = document.getElementById('canvas');
+// * variables que guardara el "contexto" de renderizado del canva y su funciones de dibujo, en este caso, un dibujo 2d
 const ctx = canvas.getContext('2d');
 const fileInput = document.getElementById("fileInput");
 const deleteBackground = document.getElementById("deleteBackground")
@@ -19,7 +21,7 @@ function runModel() {
             canvas.width = img.width;
             canvas.height = img.height;
 
-            // * Dibujar la imagen en el canvas
+            // * Dibujar la imagen en el canvas mediante el metodo drawImage, pasandolo como parametros la imagen, las coordenadas y el ancho y alto de la imagen
             ctx.drawImage(img, 0, 0, img.width, img.height);
             canvas.classList.remove("hidden")
         };
@@ -28,52 +30,60 @@ function runModel() {
 
 deleteBackground.addEventListener("click", async () => {
 
+    //* Cargar el canva original
     const originalCanvas = document.querySelector('canvas');
     const originalCtx = originalCanvas.getContext('2d');
 
-    // Crear un nuevo canvas
+    // * Crear un nuevo canvas
     const newCanvas = document.createElement('canvas');
     const newCtx = newCanvas.getContext('2d');
 
-    // Establecer el tamaño del nuevo canvas igual al del canvas original
+    // * Establecer el tamaño del nuevo canvas igual al del canvas original
     newCanvas.width = originalCanvas.width;
     newCanvas.height = originalCanvas.height;
 
-    // Agregar el nuevo canvas al DOM (por ejemplo, después del canvas original)
+    // * Agregar el nuevo canvas al DOM (por ejemplo, después del canvas original)
     originalCanvas.parentNode.appendChild(newCanvas);
 
-    // Cargar modelo
+    // * Cargar modelo y sus configuraciones
     const net = await bodyPix.load({
-        architecture: 'MobileNetV1',
+        architecture: 'MobileNetV1', // ResNet50 //* MobileNetV1
+        // * que tan precio seran, cuanto mas pequeño mas preciso pero mas pesado. 
         outputStride: 16,
-        multiplier: 0.75,
+        // * tamaño del modelo
+        // multiplier: 0.75,
+        // * el peso en bytes que usara el modelo, cuanto mas grande, mas lento redes neuronales
         quantBytes: 2
     });
 
-    // Segmentación
+    // * Segmentación de una persona en el canva.
     const { data: map } = await net.segmentPerson(originalCanvas, {
+        // * la precision, afecta la velodcidad
         internalResolution: 'medium',
     });
 
-    // Extraer data de la imagen
+    // *  Extraer data (mapa de bits) de la imagen
     const imgData = originalCtx.getImageData(0, 0, originalCanvas.width, originalCanvas.height);
 
-    // Crear nueva image data para el nuevo canvas
+    // * Crear nueva image data para el nuevo canvas
     const newImg = newCtx.createImageData(newCanvas.width, newCanvas.height);
     const newImgData = newImg.data;
 
+    // * un bucle itera sobre los pixeles copiando los pixeles que cumplan con la condicion en un nuevo canva.
     for (let i = 0; i < map.length; i++) {
-        // The data array stores four values for each pixel
+        // *  Extrae los bits segun cada color del array del mapa de bits
         const [r, g, b, a] = [imgData.data[i * 4], imgData.data[i * 4 + 1], imgData.data[i * 4 + 2], imgData.data[i * 4 + 3]];
         [
             newImgData[i * 4],
             newImgData[i * 4 + 1],
             newImgData[i * 4 + 2],
             newImgData[i * 4 + 3]
+
+            // * map[i] contiene el valor de cada pixel, 0 es el fondo y 1 es la persona
         ] = !map[i] ? [255, 255, 255, 0] : [r, g, b, a];
     }
 
-    // Dibujar la nueva imagen en el nuevo canvas
+    // * Dibujar la nueva imagen en el nuevo canvas
     newCtx.putImageData(newImg, 0, 0);
 })
 
